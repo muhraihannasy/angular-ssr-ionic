d459f0ba4dabf20a54ce9cf63796a4e049c00a07
muhraihannasy <muhraihannasy@gmail.com>
2024-04-24 10:42:34 +0700

    adding ssr for ionic

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomePageRoutingModule } from './home-routing.module';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomePageRoutingModule],
+  imports: [CommonModule, HomePageRoutingModule, IonicModule],
 })
 export class HomeModule {}
